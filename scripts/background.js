5b68676fc5320332c1bfa229e83f0ba9c767f06e
kmorebetter <141675980+kmorebetter@users.noreply.github.com>
2024-12-27 20:51:07 -0600

    Update background.js

--- scripts/background.js
+++ scripts/background.js
@@ -1,12 +1,17 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   console.log('Background script received message:', request);
+  
   if (request.action === 'captureArea') {
     chrome.tabs.captureVisibleTab(null, { format: 'png' }, dataUrl => {
+      console.log('Screenshot captured');
+      
       const img = new Image();
       
       img.onload = () => {
+        console.log('Image loaded, creating canvas');
         const canvas = document.createElement('canvas');
         const ctx = canvas.getContext('2d');
+        
         canvas.width = request.area.width;
         canvas.height = request.area.height;
         
@@ -17,17 +22,12 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
           request.area.width, request.area.height
         );
         
-        sendResponse({ imageData: canvas.toDataURL() });
+        console.log('Sending cropped image data');
+        sendResponse({ imageData: canvas.toDataURL('image/png') });
       };
 
       img.src = dataUrl;
     });
-    return true; // Will respond asynchronously
-  }
-});
-
-chrome.commands.onCommand.addListener((command) => {
-  if (command === '_execute_action') {
-    chrome.action.openPopup();
+    return true;
   }
 });
